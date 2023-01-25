52f8f7bb41d83e7707c76d24519961e202ae23f7
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2023-01-25 22:38:41 +0400

    mini programm weather

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import asyncio
-
 import time
+from aiohttp import ClientSession
 
-async def fun1(x):
-    print(x**2)
-    await asyncio.sleep(3)
-    print('fun1 complete')
+async def get_weather(city):
+    async with ClientSession() as session:
+        url = f'http://api.openweathermap.org/data/2.5/weather'
+        params = {'q': city, 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
 
-async def fun2(x):
-    print(x**0.5)
-    await asyncio.sleep(3)
-    print('fun2 complete')
+        async with session.get(url=url, params = params) as response:
+            weather_json = await response.json()
+            print(f'{city}:{weather_json["weather"][0]["main"]}')
 
-async def main():
-    task1 = asyncio.create_task(fun1(4))
-    task2 = asyncio.create_task(fun2(4))
+async def main(cities_):
+    tasks = []
+    for city in cities_:
+        tasks.append(asyncio.create_task(get_weather(city)))
 
-    await task1
-    await task2
 
+    for task in tasks:
+        await task
+
+cities = ['Moscow', 'London']
 
 print(time.strftime('%X'))
-
-asyncio.run(main())
+asyncio.run(main(cities))
 print(time.strftime('%X'))
